3e5c6808ec22345912f557f4ff91a0446f3b5132
Qassim.Dev <Qassim.Dev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-26 20:03:19 +0000

    settings time-ago
    git-svn-id: https://plugins.svn.wordpress.org/time-ago/trunk@1146415 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- settings.php
+++ settings.php
@@ -150,14 +150,14 @@
                     <p>Get collection of 87 WordPress themes for $69 only, a lot of features and free support! <a href="http://j.mp/ET_WPTime_ref_pl" target="_blank">Get it now</a>.</p>
                     <p>See also:</p>
                         <ul>
-                            <li><a href="http://j.mp/GL_WPTime" target="_blank">Best Gallery & Portfolio WordPress Plugins.</a></li>
+                            <li><a href="http://j.mp/GL_WPTime" target="_blank">Must Have Awesome Plugins.</a></li>
                             <li><a href="http://j.mp/CM_WPTime" target="_blank">Premium WordPress themes on CreativeMarket.</a></li>
                             <li><a href="http://j.mp/TF_WPTime" target="_blank">Premium WordPress themes on Themeforest.</a></li>
                             <li><a href="http://j.mp/CC_WPTime" target="_blank">Premium WordPress plugins on Codecanyon.</a></li>
                             <li><a href="http://j.mp/BH_WPTime" target="_blank">Unlimited web hosting for $3.95 only.</a></li>
                         </ul>
-                    <p><a href="http://j.mp/GL_WPTime" target="_blank" title="Download Best Gallery & Portfolio WordPress Plugins"><img src="http://content.ghozylab.com/partners/aff/images/global-aff-img.png" width="728" height="90"></a></p>
-                    <p><a href="http://j.mp/ET_WPTime_ref_pl" title="Get collection of 87 WordPress themes for $69 only" target="_blank"><img src="http://www.elegantthemes.com/affiliates/banners/570x100.jpg"></a></p>
+                    <p><a href="http://j.mp/GL_WPTime" target="_blank"><img src="http://content.ghozylab.com/partners/aff/images/global-aff-img.png" width="728" height="90"></a></p>
+                    <p><a href="http://j.mp/ET_WPTime_ref_pl" target="_blank"><img src="http://www.elegantthemes.com/affiliates/banners/570x100.jpg"></a></p>
                 </div>
             </div>
         <?php
